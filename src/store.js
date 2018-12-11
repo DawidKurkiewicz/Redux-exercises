0e29601b1dc29c79d91f20f2e0d7c8d34811c931
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-11 11:11:19 +0100

    added if to reducer

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore } from 'redux'
 
 
 
- const reducer =  () => ({
-name: 'ala'
- })
+const reducer = (state, action) => {
+    if (action.type === "FIRST_ACTION") {
+        return { firstAction: true }
+    }
+    return { firstAction: false }
+}
 
 
-
- export const store = createStore(
+export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
