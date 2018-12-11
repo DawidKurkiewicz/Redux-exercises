c7ab0f448e314b6dcbc0c4c2c7dac8ae00f791bf
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-11 12:10:21 +0100

    added dec and inc actions to counter

--- src/store.js
+++ src/store.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
+
+const FIRST_ACTION = "FIRST_ACTION"
+const INC = "INC"
+const DEC = "DEC"
 
 const INITIAL_STATE = {
     firstAction: false
 }
 
-const reducer = (state = INITIAL_STATE, action) => {
+const INITIAL_STATE2 = {
+    counter: 0
+}
+
+const firstAction = () => ({
+    type: FIRST_ACTION
+})
+const firstActionReducer = (state = INITIAL_STATE, action) => {
     if (action.type === "FIRST_ACTION") {
         return {
             ...state,
@@ -14,15 +25,41 @@ const reducer = (state = INITIAL_STATE, action) => {
     return state
 }
 
-const FIRST_ACTION = "FIRST_ACTION"
-
-
-const firstAction = () => ({
-type: FIRST_ACTION
+const inc = (value = 1) => ({
+    type: INC,
+    value
+})
+const dec = (value = 1) => ({
+    type: DEC,
+    value
 })
 
+const counterReducer = (state = INITIAL_STATE2, action) => {
+    switch (action.type) {
+        case INC:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        case DEC:
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        default:
+            return state
+    }
+}
+
+const bothReducers = combineReducers({
+    firstActionReducerName: firstActionReducer,
+    counterReducerName: counterReducer
+})
+
+
+
 export const store = createStore(
-    reducer,
+    bothReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
 
@@ -32,7 +69,11 @@ store.dispatch({
     type: "FIRST_ACTION"
 })
 
-window.dispatchFirstAction= () => store.dispatch(firstAction())
+window.dispatchFirstAction = () => store.dispatch(firstAction())
+window.dispatchIncAction = () => store.dispatch(inc())
+window.dispatchDecAction = () => store.dispatch(dec())
+
+
 
 window.dispatchFirstAction()
 window.dispatchFirstAction()
