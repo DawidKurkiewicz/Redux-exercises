daf93a27933062ac0f556a577bc8a80693179b12
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-11 09:51:36 +0100

    added store.js

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { combineReducers, createStore } from 'redux'
+
+
+
+ const reducer = combineReducers({ 
+     
+ })
+
+
+
+ export const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+
+)
